c043835c2a516926440c56800bcf4e87f03373b0
Roberto Cohen <rcclsb@gmail.com>
2015-06-22 14:45:11 -0400

    Editing run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 description "Richard Simmons. The greatest hubot of all time."
 
 chmod u+x bin/starthubot.sh
-start on filesystem or runlevel [2345]
-stop on runlevel [!2345]
-respawn
-respawn limit 5 60
- 
+
 script
 # Start Hubot
 exec bin/starthubot.sh
