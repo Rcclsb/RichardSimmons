890489106759133784ebbf12d9ff949f139ffd15
Roberto Cohen <rcclsb@gmail.com>
2015-07-06 10:19:46 -0400

    changed JIRAURL

--- bin/starthubot.sh
+++ bin/starthubot.sh
@@ -1,5 +1,5 @@
 export HUBOT_SLACK_TOKEN=xoxb-6640409751-3qWhhpd5e5qwX1ZXd9YrMzh5 
-export HUBOT_JIRA_URL="https://waybetter.atlassian.net/secure/"
+export HUBOT_JIRA_URL="https://waybetter.atlassian.net"
 export HUBOT_JIRA_USER="rcclsb@gmail.com"
 export HUBOT_JIRA_PASSWORD="roberto0417"
 echo "Starting RichardSimmons..."
